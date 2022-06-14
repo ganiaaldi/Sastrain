926274a4c5fa59c05843a7347ee684ed85c16d28
ganiaaldi <mrgad98@gmail.com>
2022-06-14 12:16:27 +0700

    Implementasi header & footer with expanded nav menu in mobile

---
+++ src/utils/script.js
@@ -0,0 +1,8 @@
+const navMenu = document.querySelector('.nav-menu span');
+const nav = document.querySelector('nav ul');
+
+    navMenu.addEventListener('click', function () {
+    nav.classList.toggle('expanded-menu');
+    });
+
+
